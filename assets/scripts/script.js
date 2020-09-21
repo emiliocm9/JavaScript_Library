2f4b76f4c926a3a850de1884c107c39bb11cb4b7
Jamezjaz <jamezjaz@gmail.com>
2020-09-21 12:09:22 -0500

    docs: displays each book on the page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,4 @@
+const myContainer = document.querySelector('.library-container');
 let myLibrary = [];
 
 function Book(title, author, year, pages, read) {
@@ -14,4 +15,19 @@ function addBookToLibrary(title, author, year, pages, read) {
   console.log(myLibrary);
 }
 
+function bookDisplay(myLibrary) {
+  myLibrary.forEach(function(book){
+    myContainer.innerHTML = `<div class="card">
+      <div class="card-body">
+        <h1 class="card-title">Title:${book.title}</h1>
+        <h1 class="card-title">Author:${book.author}</h1>
+        <h1 class="card-title">Year:${book.year}</h1>
+        <h1 class="card-title">Pages:${book.pages}</h1>
+        <h1 class="card-title">Read:${book.read}</h1>
+      </div>
+    </div>`;
+  });
+}
+
 addBookToLibrary('otherBook', 'Emilio', 2020, 234, true);
+bookDisplay(myLibrary);
