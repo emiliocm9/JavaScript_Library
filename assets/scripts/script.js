c601a10cd1588b367872b864201e8a64b3f49ea4
emiliocm9 <emilio.contreras97@gmail.com>
2020-09-21 16:56:47 -0500

    refactor: delete book method

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 const myContainer = document.querySelector('.library-container');
 
-function Book(title, author, year, pages, read) {
+function myBook(title, author, year, pages, read) {
   this.title = title;
   this.author = author;
   this.year = year;
@@ -10,39 +10,40 @@ function Book(title, author, year, pages, read) {
 }
 
 function addBookToLibrary(title, author, year, pages, read) {
-  let userBook = new Book(title, author, year, pages, read);
+  let userBook = new myBook(title, author, year, pages, read);
   myLibrary.push(userBook);
-  console.log(myLibrary);
+  bookDisplay(userBook);
 }
 
-function bookDisplay(myLibrary) {
-  myLibrary.forEach(function(book) {
-    let bookIndex = myLibrary.indexOf(book);
-    myContainer.setAttribute("data-index", `${bookIndex}`);
-    myContainer.innerHTML = `<div class="card">
-      <div class="card-body">
-        <h1 class="card-title">Title:${book.title}</h1>
-        <h1 class="card-title">Author:${book.author}</h1>
-        <h1 class="card-title">Year:${book.year}</h1>
-        <h1 class="card-title">Pages:${book.pages}</h1>
-        <h1 class="card-title">Read:${book.read}</h1>
-      </div>
+function bookDisplay(book) {
+  let bookIndex = myLibrary.indexOf(book);
+
+  let myCard = document.createElement("div");
+  myCard.className = "card w-50";
+  myCard.setAttribute("data-index", `${bookIndex}`);
+  myCard.innerHTML = `
+    <div class="card-body">
+      <h1 class="card-title">Title:${book.title}</h1>
+      <h1 class="card-title">Author:${book.author}</h1>
+      <h1 class="card-title">Year:${book.year}</h1>
+      <h1 class="card-title">Pages:${book.pages}</h1>
+      <h1 class="card-title">Read:${book.read}</h1>
     </div>
-    <button id="delete" class="remove">Delete</button>`;
-    deleteBook(book, bookIndex);
-  });
+  <button id="delete" class="remove">Delete</button>`;
+
+  deleteBook(myCard, bookIndex);
+
+  myContainer.appendChild(myCard);
 }
 
-function deleteBook(book, bookIndex) {
-  myContainer.querySelector('#delete').addEventListener("click", function(){
-    // let element = document.querySelector(`[data-index="${bookIndex}"]`);
-    myContainer.removeChild(book);
+function deleteBook(myCard, bookIndex) {
+  myCard.querySelector("#delete").addEventListener("click", function() {
+    let element = document.querySelector(`[data-index="${bookIndex}"]`);
+    myContainer.removeChild(element);
     delete myLibrary(bookIndex);
   });
-
 }
 
 
 addBookToLibrary('otherBook', 'Emilio', 2020, 234, true);
 addBookToLibrary('firstBook', 'Jamez', 1987, 536, false);
-bookDisplay(myLibrary);
