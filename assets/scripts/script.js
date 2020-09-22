75382ae79e08e69274868bd31fe2169a66df1e56
Jamezjaz <jamezjaz@gmail.com>
2020-09-22 10:46:20 -0500

    docs: Adds read status functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,10 +14,10 @@ function uploadBook() {
 
   savebutton.addEventListener("click", function() {
     let title = document.querySelector("#title").value;
-    let author = document.querySelector("#title").value;
-    let year = document.querySelector("#title").value;
-    let pages = document.querySelector("#title").value;
-    let read = document.querySelector("#title").checked;
+    let author = document.querySelector("#author").value;
+    let year = document.querySelector("#year").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
 
     addBookToLibrary(title, author, year, pages, read);
   });
@@ -41,13 +41,18 @@ function bookDisplay(book) {
       <h1 class="card-title">Author:${book.author}</h1>
       <h1 class="card-title">Year:${book.year}</h1>
       <h1 class="card-title">Pages:${book.pages}</h1>
-      <h1 class="card-title">Read:${book.read}</h1>
+      <h1 class="card-title" id="read-title">Read:${book.read}</h1>
     </div>
+    <label for="read-check">
+      <input type="checkbox" id="read-check"/><p>Read</p>
+    </label>
   <button id="delete" class="remove">Delete</button>`;
 
+  
   deleteBook(myCard, bookIndex);
 
   myContainer.appendChild(myCard);
+  toggle(book);
 }
 
 function deleteBook(myCard, bookIndex) {
@@ -56,6 +61,30 @@ function deleteBook(myCard, bookIndex) {
     myContainer.removeChild(element);
     delete myLibrary(bookIndex);
   });
+}
+
+function toggle(book) {
+  const checkBook = document.querySelector("#read-check");
+  checkBook.addEventListener("click", function(){
+    if (book.read == true) {
+      book.read = false;
+      toggleStatus(book);
+    }
+    else {
+      book.read = true;
+      toggleStatus(book);
+    }
+  });
+}
+
+function toggleStatus(book) {
+  const readTitle = document.querySelector("#read-title");
+  if (book.read) {
+    readTitle.textContent = 'Read: true';
+  }
+  else {
+    readTitle.textContent = 'Read: false';
+  }
 }
 
 uploadBook();
