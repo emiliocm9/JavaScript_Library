365d66372f7117a3634fe93b44451c4e4ffd7c65
emiliocm9 <emilio.contreras97@gmail.com>
2020-09-22 17:23:57 -0500

    style: correct linter errors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 const myLibrary = [];
 const myContainer = document.querySelector('.library-container');
 
-function myBook(title, author, year, pages, read) {
+function MyBook(title, author, year, pages, read) {
   this.title = title;
   this.author = author;
   this.year = year;
@@ -9,54 +9,18 @@ function myBook(title, author, year, pages, read) {
   this.read = read;
 }
 
-function toggleCheckBox() {
-  const card = myCard.querySelector('.card-body');
-  if (book.read) {
-    checkBook.checked = true;
-    card.classList.remove('bg-info');
-    card.classList.add('bg-success');
-    nameToggle(readText)
-  } else {
-    checkBook.checked = false;
-    card.classList.remove('bg-success');
-    card.classList.add('bg-info');
-    nameToggle(readText)
-  }
-}
-
-function nameToggle(readText) {
-  if (book.read) {
-    readText.textContent = 'Read: true';
-  } else {
-    readText.textContent = 'Read: false';
-  }
-}
-
-
-function uploadBook() {
-  const savebutton = document.querySelector('#add-button')
-
-  savebutton.addEventListener('click', function() {
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const year = document.querySelector('#year').value;
-    const pages = document.querySelector('#pages').value;
-    const read = document.querySelector('#read').checked;
-
-    addBookToLibrary(title, author, year, pages, read);
+function deleteBook(myCard, bookIndex) {
+  myCard.querySelector('#delete').addEventListener('click', () => {
+    const element = document.querySelector(`[data-index="${bookIndex}"]`);
+    myContainer.removeChild(element);
+    delete myLibrary(bookIndex);
   });
 }
 
-function addBookToLibrary(title, author, year, pages, read) {
-  let userBook = new myBook(title, author, year, pages, read);
-  myLibrary.push(userBook);
-  bookDisplay(userBook);
-}
-
 function bookDisplay(book) {
-  let bookIndex = myLibrary.indexOf(book);
+  const bookIndex = myLibrary.indexOf(book);
 
-  let myCard = document.createElement('div');
+  const myCard = document.createElement('div');
   myCard.className = 'card w-50 p-4';
   myCard.setAttribute('data-index', `${bookIndex}`);
   myCard.innerHTML = `
@@ -77,15 +41,37 @@ function bookDisplay(book) {
 
   const checkBook = myCard.querySelector('.read-check');
   const readText = myCard.querySelector('.read-title');
+
+  function nameToggle(readText) {
+    if (book.read) {
+      readText.textContent = 'Read: true';
+    } else {
+      readText.textContent = 'Read: false';
+    }
+  }
+
+  function toggleCheckBox() {
+    const card = myCard.querySelector('.card-body');
+    if (book.read) {
+      checkBook.checked = true;
+      card.classList.remove('bg-info');
+      card.classList.add('bg-success');
+      nameToggle(readText);
+    } else {
+      checkBook.checked = false;
+      card.classList.remove('bg-success');
+      card.classList.add('bg-info');
+      nameToggle(readText);
+    }
+  }
+
   toggleCheckBox();
   nameToggle(readText);
-
-  checkBook.addEventListener('change', function() {
+  checkBook.addEventListener('change', () => {
     if (book.read) {
       book.read = false;
       toggleCheckBox();
-    }
-    else {
+    } else {
       book.read = true;
       toggleCheckBox();
     }
@@ -94,11 +80,23 @@ function bookDisplay(book) {
   myContainer.appendChild(myCard);
 }
 
-function deleteBook(myCard, bookIndex) {
-  myCard.querySelector('#delete').addEventListener('click', function() {
-    let element = document.querySelector(`[data-index="${bookIndex}"]`);
-    myContainer.removeChild(element);
-    delete myLibrary(bookIndex);
+function addBookToLibrary(title, author, year, pages, read) {
+  const userBook = new MyBook(title, author, year, pages, read);
+  myLibrary.push(userBook);
+  bookDisplay(userBook);
+}
+
+function uploadBook() {
+  const savebutton = document.querySelector('#add-button');
+
+  savebutton.addEventListener('click', () => {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const year = document.querySelector('#year').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').checked;
+
+    addBookToLibrary(title, author, year, pages, read);
   });
 }
 
